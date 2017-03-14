1665cc895a8cd5ddc8b128baff3c81cda9176192
JP Marra <jp.marra@me.com>
2017-03-14 18:55:56 -0400

    events are clickable from results now

--- client/app/results/results.js
+++ client/app/results/results.js
@@ -1,6 +1,5 @@
 angular.module('lookoutApp.results', [])
 .controller('resultsCtrl', function($scope, $location, resultsFactory, dataFactory, eventFactory) {
-  console.log(resultsFactory.results.data)
   $scope.results = resultsFactory.results.data;
   $scope.openEvent = function(event){
     eventFactory.insertEvent(event);
