2f2c5c4f0ad3b49f344ad23f338d5534f5af4a48
JP Marra <jp.marra@me.com>
2017-03-15 14:50:44 -0400

    Working on results render

--- client/app/event/event.js
+++ client/app/event/event.js
@@ -1,7 +1,6 @@
 angular.module('lookoutApp.event', [])
-.controller('eventCtrl', function($scope, $sce, eventFactory, dataFactory) {
+.controller('eventCtrl', function($scope, $window, $sce, eventFactory, dataFactory) {
   $scope.event = eventFactory.event;
-  console.log($scope.event)
   $scope.escapedAddress = $scope.event.venueAddress.split(" ").join("%20");
   $scope.untrustedMapUrl = "https://www.google.com/maps/embed/v1/place?key=AIzaSyDanwxT0CdlMhsj0D2Yn-t6gNZ6K3_Pjfs&q="+$scope.escapedAddress;
   $scope.mapUrl = $sce.trustAsResourceUrl($scope.untrustedMapUrl)
