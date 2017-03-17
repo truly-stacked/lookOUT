28be109ca98ad18cf403cc14e977eaec9f2cc702
Michael Chan <michaelchan@Michaels-Mac.local>
2017-03-17 09:40:12 -0400

    exchanged nodemon to node

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require ('express'),
   morgan = require ('morgan'),
-  bodyParser = require('body-parser');
-  
+  bodyParser = require('body-parser'),
   app = express();
 
   app.use(morgan('dev'));
